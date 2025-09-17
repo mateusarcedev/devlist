f9eb201e339bf1e74aa322cc58fba22a78908972
Mateus Arce <arcemateuss@gmail.com>
2025-09-16 21:45:15 -0400

    Refine favorites controller concurrency spec setup

--- api/src/favorites/favorites.controller.spec.ts
+++ api/src/favorites/favorites.controller.spec.ts
@@ -1,22 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { FavoritesController } from './favorites.controller';
 import { FavoritesService } from './favorites.service';
 import { FakePrismaService } from './testing/fake-prisma.service';
 
 describe('FavoritesController', () => {
+  let moduleRef: TestingModule;
   let controller: FavoritesController;
-  let service: FavoritesService;
   let prisma: FakePrismaService;
 
   const userId = 123;
   const toolId = '00000000-0000-0000-0000-000000000001';
 
-  beforeEach(() => {
+  beforeEach(async () => {
     prisma = new FakePrismaService();
     prisma.seedUser(userId);
     prisma.seedTool(toolId);
 
-    service = new FavoritesService(prisma as any);
-    controller = new FavoritesController(service);
+    moduleRef = await Test.createTestingModule({
+      controllers: [FavoritesController],
+      providers: [
+        FavoritesService,
+        {
+          provide: PrismaService,
+          useValue: prisma,
+        },
+      ],
+    }).compile();
+
+    controller = moduleRef.get(FavoritesController);
+  });
+
+  afterEach(async () => {
+    await moduleRef?.close();
   });
 
   it('should be defined', () => {
